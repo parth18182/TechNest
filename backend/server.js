4cc3a602e63090287a2b241afae0c77cc95b56ff
parth18182 <panchalparth1218@gmail.com>
2026-05-16 13:24:25 +0530

    TechNest MERN App

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,25 @@
-import dotenv from 'dotenv';
-dotenv.config(); 
-import express from 'express';
-import connection from './database/db.js';
-import cors from 'cors'
-import userRoute from './routes/userRoute.js'
-import productRoute from './routes/productRoute.js';
-import cartRoute from './routes/cartRoute.js'
-import orderRoute from './routes/orderRoute.js'
-
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import connection from "./database/db.js";
+import cors from "cors";
+import userRoute from "./routes/userRoute.js";
+import productRoute from "./routes/productRoute.js";
+import cartRoute from "./routes/cartRoute.js";
+import orderRoute from "./routes/orderRoute.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+app.use(cors());
 
-app.use('/api/users',userRoute)
-app.use('/api/products',productRoute)
-app.use('/api/cart',cartRoute)
-app.use('/api/orders',orderRoute)
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(PORT, async () => {
-    await connection();
-    console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+  await connection();
+  console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
 });
